7a655cb31de135754a6b3b68c4a7523d780f062d
ariyeb <ariyeb@gmail.com>
2020-10-13 19:43:52 +0300

    usersRouter

--- src/routers/usersRouter.js
+++ src/routers/usersRouter.js
@@ -1,7 +1,38 @@
 const express = require("express");
+const User = require("../models/userModel");
 
 const router = new express.Router();
 
+router.post("/users/new", async (req, res) => {
+	const user = new User(req.body);
+	try {
+		await user.save();
+		res.send(user);
+	} catch (err) {
+		res.status(400).send({
+			status: 400,
+			message: err.message,
+		});
+	}
+});
 
+router.get("/users/get", async (req, res) => {
+	const _id = req.query.id;
+	try {
+		const user = await User.findById(_id);
+		if (!user) {
+			return res.status(404).send({
+				status: 404,
+				message: "wrong id",
+			});
+		}
+
+		res.send(user);
+	} catch (err) {
+		res.status(500).send(err);
+	}
+});
+
+// צרו ראוט לקבלת כל היוזרים
 
 module.exports = router;

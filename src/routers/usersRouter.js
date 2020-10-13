28f40db4099716ca151c4867c543c6276343ee0f
ariyeb <ariyeb@gmail.com>
2020-10-13 20:14:13 +0300

    patch user

--- src/routers/usersRouter.js
+++ src/routers/usersRouter.js
@@ -33,6 +33,46 @@ router.get("/users/get", async (req, res) => {
 	}
 });
 
-// צרו ראוט לקבלת כל היוזרים
+router.get("/users/get-all", async (req, res) => {
+	try {
+		const users = await User.find({});
+		if (users.length === 0) {
+			return res.status(404).send({
+				status: 404,
+				message: "no users",
+			});
+		}
+		res.send(users);
+	} catch (err) {
+		res.status(500).send(err);
+	}
+});
+
+router.patch("/users/edit", async (req, res) => {
+	const _id = req.query.id;
+
+	try {
+		const user = await User.findByIdAndUpdate(_id, req.body, {
+			new: true,
+			runValidators: true,
+		});
+
+		if (!user) {
+			return res.status(404).send({
+				status: 404,
+				message: "wrong id",
+			});
+		}
+
+		res.send(user);
+	} catch (err) {
+		res.status(400).send({
+			status: 400,
+			message: err.message,
+		});
+	}
+});
+
+// תרגיל - צרו קוד שמוודא שבתוך גוף הבקשה יש רק שדות מותרים לעדכון
 
 module.exports = router;
